52ec5b224c9b0bf813f6c67516cdc46873e0cc3d
Adley <adley.salabi@gmail.com>
2020-07-20 22:35:51 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+</body>
+<script type="text/javascript" src="./js/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="./js/main.js"></script>
+</html>
---
+++ menu.php
@@ -0,0 +1,28 @@
+<section class="section_menu">
+    <div class="header">
+        <span>Magnity</span>
+        <button class="hamburger">&#9776;</button>
+        <button class="cross">&#735;</button>
+    </div>
+
+    
+
+    <div class="menu">
+        <ul>
+            <a href="#">
+                <li>link 1</li>
+            </a>
+            <a href="#">
+                <li>link 2</li>
+            </a>
+            <a href="#">
+                <li>link 3</li>
+            </a>
+            <a href="#">
+                <li>link 4</li>
+            </a>
+
+        </ul>
+    </div>
+
+</section >
